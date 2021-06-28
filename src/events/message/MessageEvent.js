fd4a4029069bd5fcd041283d25c44885eea05b58
notbozho <bozhoivanow@gmail.com>
2021-06-28 08:34:23 +0000

    disable commands in dms

--- src/events/message/MessageEvent.js
+++ src/events/message/MessageEvent.js
@@ -1,6 +1,8 @@
 const BaseEvent = require('../../utils/structures/BaseEvent');
 const config = require('../../../slappey.json');
 
+const { MessageEmbed } = require('discord.js');
+
 module.exports = class MessageEvent extends BaseEvent {
   constructor() {
     super('message');
@@ -8,12 +10,24 @@ module.exports = class MessageEvent extends BaseEvent {
   
   async run(client, message) {
     if (message.author.bot) return;
-    const { prefix } = client.configs.get(message.guild.id);
+
+    let prefix;
+    if (message.channel.type == 'text' || message.channel.type == 'news') {
+        prefix = client.configs.get(message.guild.id).prefix;
+    } else if (message.channel.type == 'dm') {
+        prefix = 'b!';
+        message.reply('Commands are disabled in dms');
+        return;
+    } else {
+        return;
+    }
+
     if (message.content.startsWith(prefix)) {
       const [cmdName, ...cmdArgs] = message.content
       .slice(prefix.length)
       .trim()
       .split(/\s+/);
+
       const command = client.commands.get(cmdName);
       if (command) {
         command.run(client, message, cmdArgs);
